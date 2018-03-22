6656e705df102dcb88e39f70430023cba510a397
lengqiufeifeng@sina.cn <qiufeng_1008>
2018-03-22 15:44:27 +0800

    修改配置

--- service-user/src/main/java/logan/exemple/service/base/DataSourceConfiguration.java
+++ service-user/src/main/java/logan/exemple/service/base/DataSourceConfiguration.java
@@ -1,8 +1,9 @@
 package logan.exemple.service.base;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
